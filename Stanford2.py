ff418eea3b7c24e57c91fab5bf805ec3f1efc403
Nele Albers <nele.96@web.de>
2019-03-28 15:19:14 +0100

    updated stanford

--- Stanford2.py
+++ Stanford2.py
@@ -39,6 +39,17 @@ def get_max_length_syntactic_dependencies(text):
             maxD = dist
     return [maxD]
 
+def get_avg_length_syntactic_dependencies(text):
+    "Returns the average distance between the governing and the dependent words \
+    in terms of the number of words separating them."
+    parse = next(parser.raw_parse(text))
+    totalD = 0 # sum of distances between governing and dependent words
+    numEntries = len(parse.nodes) # number of entries in dependency tree
+    for entry in range(1,  numEntries): # for each term in the sentence
+        dist = abs(entry - parse.nodes[entry]["head"])
+        totalD += dist
+    return [totalD / (numEntries - 1)]
+
 if __name__ == '__main__':
     print(get_max_length_syntactic_dependencies("I put the book in the box on the table"))
     server.stop()
