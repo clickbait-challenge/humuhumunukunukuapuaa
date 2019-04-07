3fe1d6b89dca31281184372d9ecbc247d3d452f0
mihaivo <mihai1voicescu@gmail.com>
2019-04-07 19:13:48 +0200

    Format

--- features/change_dataset.py
+++ features/change_dataset.py
@@ -1,27 +1,25 @@
 import json
 import os
 
-
 CWD = os.getcwd()
 JSON_CONFIG_FILE_PATH = '%s/%s' % (CWD, 'twitter-out2019-04-06T12_10_16.106Z.jsonl');
 CONFIG_PROPERTIES = {}
 with open(JSON_CONFIG_FILE_PATH, 'r', encoding="utf8") as f:
     data = [json.loads(line) for line in f]
 
-	
 print(data[0])
-	
-for i in range(0,len(data)):
-	if(data[i]['postMedia']):
-		first = data[i]['postMedia'][0];
-		data[i]['id'] = first.split('.')[0]
-		
+
+for i in range(0, len(data)):
+    if (data[i]['postMedia']):
+        first = data[i]['postMedia'][0];
+        data[i]['id'] = first.split('.')[0]
+
 print(data[8]['id'])
 print(data[8]['postMedia'])
 
 print(len(data))
 
-for i in range(0,len(data)):
-	with open('instances_modified.jsonl', 'a') as outfile:  
-		json.dump(data[i], outfile)
-		outfile.write('\n')
+for i in range(0, len(data)):
+    with open('instances_modified.jsonl', 'a') as outfile:
+        json.dump(data[i], outfile)
+        outfile.write('\n')
--- features/text_image_extract.py
+++ features/text_image_extract.py
@@ -2,41 +2,47 @@ from PIL import Image
 
 import glob
 import cv2
+
 try:
     from PIL import Image
 except ImportError:
     import Image
 import pytesseract
 
-#the two functions of counting words and chars
+
+# the two functions of counting words and chars
 
 def word_count_function(string):
-	number_of_words = len(string.split())
-	return number_of_words;
-	
-	
+    number_of_words = len(string.split())
+    return number_of_words;
+
+
 def character_count_function(string):
-	number_of_chars = len(string);
-	return number_of_chars;
-	
-#apply pytesseract on the 
+    number_of_chars = len(string);
+    return number_of_chars;
+
+
+# apply pytesseract on the
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-all_files_with_path = glob.glob("D:/msc/q3/information retrieval/papers to review/nlp project/train4/custom/media/*.jpg") + glob.glob("D:/msc/q3/information retrieval/papers to review/nlp project/train4/custom/media/*.png")
+all_files_with_path = glob.glob(
+    "D:/msc/q3/information retrieval/papers to review/nlp project/train4/custom/media/*.jpg") + glob.glob(
+    "D:/msc/q3/information retrieval/papers to review/nlp project/train4/custom/media/*.png")
 print(len(all_files_with_path))
 image_text_no_words = [];
 image_text_no_chars = [];
 for i in range(0, len(all_files_with_path)):
-	if(pytesseract.image_to_string(Image.open(all_files_with_path[i]))):
-		image_text_no_words.append(word_count_function(pytesseract.image_to_string(Image.open(all_files_with_path[i]))));
-		image_text_no_chars.append(character_count_function(pytesseract.image_to_string(Image.open(all_files_with_path[i]))));
-	else:
-		image_text_no_words.append('0');
-		image_text_no_chars.append('0');
+    if (pytesseract.image_to_string(Image.open(all_files_with_path[i]))):
+        image_text_no_words.append(
+            word_count_function(pytesseract.image_to_string(Image.open(all_files_with_path[i]))));
+        image_text_no_chars.append(
+            character_count_function(pytesseract.image_to_string(Image.open(all_files_with_path[i]))));
+    else:
+        image_text_no_words.append('0');
+        image_text_no_chars.append('0');
 with open('text_image_words_no.txt', 'w') as f:
-	for item in image_text_no_words:
-		f.write("%s\n" % item)
+    for item in image_text_no_words:
+        f.write("%s\n" % item)
 with open('text_image_chars_no.txt', 'w') as f:
-	for item in image_text_no_chars:
-		f.write("%s\n" % item)
-			
+    for item in image_text_no_chars:
+        f.write("%s\n" % item)
